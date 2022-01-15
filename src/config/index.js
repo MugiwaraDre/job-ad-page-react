4db780269050a967952f1d6c7f143469c180d67c
MugiwaraDre <atd.hirsch@gmail.com>
2022-01-15 13:22:49 +0100

    [Dre] | added baseUrl

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+const urls = new Map();
+
+urls.set("localhost", "http://localhost:5000");
+urls.set("job-page.herokuapp.com/", "https://fastify-course.herokuapp.com/");
+
+export const baseUrl = urls.get(window.location.hostname);
